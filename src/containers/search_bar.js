bba578958e9a920bf0e1536b03a78d0bb45a5a0f
SinaDeyhim <sina.deyhim@gmail.com>
2018-07-04 12:12:06 -0400

    finished weather

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -33,7 +33,7 @@ class SearchBar extends Component{
     return(
       <form onSubmit= {this.onFormSubmit} className="input-group">
         <input
-          placeholder= "Get a five-day forecast in your favorite cities"
+          placeholder= "Get a five-day forecast in your favorite Canadian cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange}/>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component{
@@ -11,12 +12,16 @@ class WeatherList extends Component{
     const temps = cityData.list.map(weather=>weather.main.temp);
     const pressures = cityData.list.map(weather=>weather.main.pressure);
     const humidities = cityData.list.map(weather=>weather.main.humidity);
+    //const lon = cityData.city.coord.lon;
+    //const lat = cityData.city.coord.lat; es6 version
+    const {lon, lat} =cityData.city.coord;
+
     return(
       <tr key ={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="gold"/></td>
-        <td><Chart data={pressures} color="silver"/></td>
-        <td><Chart data={humidities} color="pink"/></td>
+        <td><GoogleMap lon={lon} lat={lat}  /></td>
+        <td><Chart data={temps} color="gold" units='&#176;C'/></td>
+        <td><Chart data={pressures} color="silver " units='hPa' /></td>
+        <td><Chart data={humidities} color="pink"units='%'/></td>
       </tr>
     );
   }
@@ -27,9 +32,9 @@ class WeatherList extends Component{
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (&#176;C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
